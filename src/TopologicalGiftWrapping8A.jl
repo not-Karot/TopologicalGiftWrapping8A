f9583beab1761230efa41687b044bdd452837452
not-Karot <52074617+not-Karot@users.noreply.github.com>
2022-06-17 13:30:29 +0200

    added notebooks

--- src/TopologicalGiftWrapping8A.jl
+++ src/TopologicalGiftWrapping8A.jl
@@ -1,5 +1,15 @@
 module TopologicalGiftWrapping8A
 
-greet() = print("Hello World!")
+using LinearAlgebraicRepresentation
+using IntervalTrees
+using NearestNeighbors
+using Triangulate
+using SparseArrays
+using LinearAlgebra
+using Distributed
+Lar = LinearAlgebraicRepresentation
+
+include("./spatial_arrangement.jl")
+export spatial_arrangement
 
 end # module
